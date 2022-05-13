474d43dfb97c655ead23d20742d36250e9d097b8
MuhammadAmirVohra <amirarif28@gmail.com>
2022-05-13 21:22:17 +0500

    node server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(process.env.port || process.env.PORT || 3000, () => {
+    console.log("Application started and Listening on port 3000");
+});
+
+// serve your css as static
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
