d89b932baff54acce78cc828ec7a07ba157e6ed1
ramshaanadeem <ramshaanadeem5@gmail.com>
2022-05-13 19:49:20 +0500

    code
    test

---
+++ js/6788.js
@@ -0,0 +1,3 @@
+jQuery(function(t){t(".lost_reset_password").on("submit",function(){t('button[type="submit"]',this).attr("disabled","disabled")})});
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
